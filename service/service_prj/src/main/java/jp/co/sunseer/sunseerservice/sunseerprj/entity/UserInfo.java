467ff872f90ffe3ace7b2e1089e03c2474a33c51
pilgrim81 <zhaoying19811009@gmail.com>
2021-05-18 13:24:24 +0900

    backend api update

--- service/service_prj/src/main/java/jp/co/sunseer/sunseerservice/sunseerprj/entity/UserInfo.java
+++ service/service_prj/src/main/java/jp/co/sunseer/sunseerservice/sunseerprj/entity/UserInfo.java
@@ -1,7 +1,7 @@
 package jp.co.sunseer.sunseerservice.sunseerprj.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.io.Serializable;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
@@ -11,16 +11,16 @@ import lombok.experimental.Accessors;
 
 /**
  * <p>
- * 
+ *
  * </p>
  *
  * @author Group AB
- * @since 2021-05-18
+ * @since 2021-05-17
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
 @Accessors(chain = true)
-@ApiModel(value="UserInfo对象", description="")
+@ApiModel(value="UserInfoオブジェクト", description="ユーザー情報")
 public class UserInfo implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -38,6 +38,9 @@ public class UserInfo implements Serializable {
     @ApiModelProperty(value = "メアド")
     private String email;
 
+//    @TableField(fill = FieldFill.INSERT)
+    @ApiModelProperty(value = "ロジック削除")
+    @TableLogic
     private Boolean isDeleted;
 
 
